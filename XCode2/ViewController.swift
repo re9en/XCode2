c999f3b2d8cc30ceaf57356507a5fb918813b93c
re9en <regen69neger@gmail.com>
2021-03-21 19:15:37 +0200

    Initial Commit

---
+++ XCode2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCode2
+//
+//  Created by admin2 on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
